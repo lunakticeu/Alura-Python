80d8637a58dadf50f14db68a4bfae8ae2695cf98
Luna <lunakticeu@gmail.com>
2024-11-25 12:26:17 -0300

    add fatorial and palindrome exercises

---
+++ Exercises/palindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(string):
+    reversed_string = string[::-1]
+    return string == reversed_string
+
+word = input('Palindrome check: Type a word ')
+if is_palindrome(word):
+    print(f"{word} is a palindrome")
+else:
+    print(f"{word} is not a palindrome")
